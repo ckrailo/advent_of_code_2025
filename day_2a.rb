c5153df1b3c97097fafc0d5ac1e08c363fb70363
Christopher Krailo <ckrailo@gmail.com>
2025-12-03 00:39:28 -0800

    Day 2: https://adventofcode.com/2025/day/2

---
+++ day_2a.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'bigdecimal'
+require 'humanize'
+
+puts 'Paste ID Ranges ...'
+
+invalid_ids_added = 0
+
+# Example Input: 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124 # rubocop:disable Metrics/LineLength
+input = $stdin.gets.chomp
+
+puts
+
+input.split(',').each do |input_range|
+  range_start, range_end = input_range.split('-').map(&:to_i)
+
+  invalid_ids = []
+
+  (range_start..range_end).each do |id|
+    id = id.to_s
+    next unless id.size > 1
+    if id[..id.size / 2 - 1] == id[id.size / 2..]
+      invalid_ids << id
+      invalid_ids_added += id.to_i
+    end
+  end
+
+  print "- #{range_start}-#{range_end}"
+
+  if invalid_ids.empty?
+    puts ' contains no invalid IDs.'
+  else
+    puts " has #{invalid_ids.size.humanize} invalid ID#{'s' if invalid_ids.size > 1}, #{invalid_ids.join(' and ')}."
+  end
+end
+
+puts "\nAdding up all the invalid IDs produces #{invalid_ids_added}."
